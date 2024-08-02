876221daff5c7aa014fefff5d41b1ca163ca4426
poovarasansivakumar2003 <poovarasansivakumar2003@gmail.com>
2024-08-02 14:52:05 +0000

    Adding file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import SudokuBoard from "./components/SudukoBoard.js";
+import { validateSudoku } from "./utils.js";
+
+const initialBoard = [
+  [5, 3, null, null, 7, null, null, null, null],
+  [6, null, null, 1, 9, 5, null, null, null],
+  [null, 9, 8, null, null, null, null, 6, null],
+  [8, null, null, null, 6, null, null, null, 3],
+  [4, null, null, 8, null, 3, null, null, 1],
+  [7, null, null, null, 2, null, null, null, 6],
+  [null, 6, null, null, null, null, 2, 8, null],
+  [null, null, null, 4, 1, 9, null, null, 5],
+  [null, null, null, null, 8, null, null, 7, 9],
+];
 
 function App() {
+  const [board, setBoard] = useState(initialBoard);
+
+  const handleInputChange = (row, col, value) => {
+    const updatedBoard = [...board];
+    updatedBoard[row][col] = value ? parseInt(value) : null;
+    setBoard(updatedBoard);
+  };
+
+  const handleSubmit = () => {
+    if (validateSudoku(board)) {
+      alert("Congratulations! You solved the Sudoku.");
+    } else {
+      alert("There are errors in your solution. Please try again.");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sudoku Game</h1>
+      <SudokuBoard board={board} onInputChange={handleInputChange} />
+      <button onClick={handleSubmit}>Check Solution</button>
     </div>
   );
 }
